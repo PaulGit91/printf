0fd05150760519a7fca4f5cce6c5558a22a563a2
PaulGit91 <paula91github@gmail.com>
2022-11-21 07:24:27 -0800

    0 test

--- main.h
+++ main.h
@@ -2,22 +2,53 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 /**
- * struct format - This is a typedef struct.
- * @opt: Pointer type char.
- * @call_fun: Pointer to call the function.
+ * Flags
  */
-typedef struct format
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/**
+ * Sizes
+ */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct format - struct op
+ * @format: the format
+ * @fun: function associated
+ */
+struct format
 {
-	char *opt;
-	int (*call_fun)();
-} format_t;
+	char format;
+	int (*fun)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct format format_t - Struct op
+ * @format: the format
+ * @format_t: function associated
+ */
+typedef struct format format_t;
 
 int _printf(const char *format, ...);
-int parse_format(const char *format, format_t get_opt[], va_list list);
-int set_string(va_list list);
-int set_char(va_list list);
-int print_percent(__attribute__((unused))va_list list);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/**
+* Functions to handle other specifiers
+*/
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 #endif
