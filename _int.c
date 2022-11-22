d758021f65bc2280ce762f97c87a1c96713d34bb
PaulGit91 <paula91github@gmail.com>
2022-11-22 03:17:08 -0800

    Betty style

--- _int.c
+++ _int.c
@@ -9,9 +9,9 @@
 int print_integer(va_list list)
 {
 	int num_length;
-	
+
 	num_length = print_integer(list);
-	
+
 	return (num_length);
 }
 
@@ -24,43 +24,41 @@ int print_integer(va_list list)
 int print_number(va_list args)
 {
 	int n;
-	
+
 	int div;
-	
+
 	int len;
-	
+
 	unsigned int num;
 
 	n  = va_arg(args, int);
-	
+
 	div = 1;
-	
+
 	len = 0;
 
 	if (n < 0)
 	{
 		len += putchar('-');
-		
+
 		num = n * -1;
 	}
-	
+
 	else
 		num = n;
 
 	for (; num / div > 9; )
-		
+
 		div *= 10;
-	
 
 	for (; div != 0; )
 	{
 		len += putchar('0' + num / div);
-		
+
 		num %= div;
-		
+
 		div /= 10;
 	}
-	
+
 	return (len);
-	
 }
